6d57161f6fb435713d89d7451ee359988a77b321
rwan <myknaifu@gmail.com>
2024-02-19 03:33:35 -0500

    removed lockfile and change configuration.nix

--- core/configuration.nix
+++ core/configuration.nix
@@ -38,12 +38,6 @@
       experimental-features = [ "nix-command" "flakes" ];
       builders-use-substitutes = true;
     };
-    registry = {
-      shells.to = {
-        type = "path";
-        path = builtins.toString ../shells;
-      };
-    };
     gc = {
       automatic = true;
       dates = "weekly";
