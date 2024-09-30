533392b7209e099ab4648fc50ccd49609c7e6706
rwan <myknaifu@gmail.com>
2024-09-30 00:54:47 -0400

    added

--- dotfiles/nvimdot/lua/plugins/colorscheme.lua
+++ dotfiles/nvimdot/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 local colorscheme = require("util.colorscheme")
 
 local gruvbox = function()
-	-- vim.o.background = "dark" -- or "light" for light mode
+	vim.o.background = "dark" -- or "light" for light mode
 	vim.g.gruvbox_material_foreground = "material"
 	vim.g.gruvbox_material_background = "medium"
-	vim.g.gruvbox_material_transparent_background = 2
+	vim.g.gruvbox_material_transparent_background = 1
+	vim.g.gruvbox_material_current_word = "grey background"
 	vim.g.gruvbox_material_diagnostic_line_highlight = 1
 	vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 	vim.g.gruvbox_material_enable_italic = 1
--- dotfiles/nvimdot/lua/plugins/noice.lua
+++ dotfiles/nvimdot/lua/plugins/noice.lua
@@ -26,6 +26,13 @@ local config = function()
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
+		views = {
+			mini = {
+				win_options = {
+					winblend = 0,
+				},
+			},
+		},
 	})
 end
 
